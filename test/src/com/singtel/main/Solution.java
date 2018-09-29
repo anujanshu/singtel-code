41d993dcbe2af32060cac784b4a69e816843135b
ANUJ <anuj.anshu@gmail.com>
2018-09-29 12:45:27 +0530

    A3. Implementation for rooster.

--- test/src/com/singtel/main/Solution.java
+++ test/src/com/singtel/main/Solution.java
@@ -3,16 +3,12 @@ package com.singtel.main;
 import com.singtel.bo.Bird;
 import com.singtel.bo.Chicken;
 import com.singtel.bo.Duck;
+import com.singtel.decorator.impl.RoosterDecorator;
 
 public class Solution {
 	
 	public static void main(String[] args) {
-		Duck duck = new Duck();
-		duck.doSpeak();
-		duck.doSwim();
-		
-		Chicken chicken = new Chicken();
-		chicken.doSpeak();
-		chicken.fly();
+		RoosterDecorator rooster = new RoosterDecorator();
+		rooster.doSpeak();
 	}
 }
